c844183e32e10a6cb2e63a5c7464a46edfd81cd7
Ktgtech <1903@holbertonschool.com>
2020-06-10 22:08:33 +0000

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,14 @@
  *main - Entry point
  *Return: Always 0 (Success)
  */
-ini main(void)
-{	printf("Size of a char: %zu bytes\n", sizeof(char));
+
+int main(void)
+{
+	printf("Size of a char: %zu bytes\n", sizeof(char));
 	printf("Size of an int: %zu bytes\n", sizeof(int));
 	printf("Size of a long: %zu bytes\n", sizeof(long));
 	printf("Size of a long long: %zu byte\n", sizeof(long long));
 	printf("Size of a float: %zu byte\n", sizeof(float));
 
-	Return (0);
+	return (0);
 }
