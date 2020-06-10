283468f59cd473b72e7fa20173b26d291bc37010
Ktgtech <1903@holbertonschool.com>
2020-06-10 20:01:14 +0000

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+  /* Prints the text inside the function puts */
   puts("\"Programming is like building a multilingual puzzle");
 
-  return 0;
+  return (0);
 }
