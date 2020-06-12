b3e12ebdb89f6c2bb8fd12ab724f7e6a86216705
Ktgtech <1903@holbertonschool.com>
2020-06-12 00:56:25 +0000

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		putchar(al);
+		al++;
+	       }
+	putchar(10);
+	return (0);
+}
