d360726df67a829882a88ea60cc107d9d7d7855e
Ktgtech <1903@holbertonschool.com>
2020-06-16 19:23:37 +0000

    task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	int a = 'a';
+
+	while ( a <= 'z')
+	{
+
+		_putchar(a);
+		a++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
