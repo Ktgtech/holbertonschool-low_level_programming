587b6b136f155838b62f46f75dfd3fe388ce2519
Ktgtech <1903@holbertonschool.com>
2020-06-16 17:50:00 +0000

    task

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *main - Entry point
+ *_putchar - for bring the word
+ *Return: Always 0 (Success)
+ */
+
+int main()
+{
+
+		_putchar('H');
+		_putchar('o');
+		_putchar('l');
+		_putchar('b');
+		_putchar('e');
+		_putchar('r');
+		_putchar('t');
+		_putchar('o');
+		_putchar('n');
+		_putchar('\n');
+
+	return (0);
+}
