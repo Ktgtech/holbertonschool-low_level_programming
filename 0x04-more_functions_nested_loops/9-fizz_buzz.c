ace9adad41b0493163be76457e16e5c7eb7a599c
Ktgtech <1903@holbertonschool.com>
2020-06-19 03:32:40 +0000

    task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,32 +11,29 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a != 100)
-
+		if ((a % 3 == 0) && (a % 5 == 0))
 		{
-
-			if ((a % 3 == 0) && (a % 5 != 0))
-			{
-				printf("Fizz ");
-			}
-			else if (a % 5 == 0)
-			{
-				printf("Buzz ");
-			}
-
-			else if (a % 3 == 0)
-			{
-				printf("FizzBuzz ");
-			}
-
-			else
-				printf("%d ", a);
+			printf("FizzBuzz ");
 		}
+		else if ((a % 5 == 0) && (a != 100))
+		{
+			printf("Buzz ");
+		}
+		else if ((a % 5 == 0) && (a == 100))
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
 		else
 		{
-			printf("Buzz");
+			printf("%d ", a);
 		}
 	}
+
 
 	printf("\n");
 
