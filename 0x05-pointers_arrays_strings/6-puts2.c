9628e29480bd35d458e8069bb496b888d7a8e6e1
Ktgtech <1903@holbertonschool.com>
2020-06-24 03:46:18 +0000

    task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,12 +7,13 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int aux = 0;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] % 2 == 0)
-
+		if (aux % 2 == 0)
 			_putchar(str[i]);
+		aux++;
 		i++;
 	}
 	_putchar('\n');
