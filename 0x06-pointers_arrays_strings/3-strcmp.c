94fe0fb3156abb9229bf70f2afdda62849217759
Ktgtech <1903@holbertonschool.com>
2020-06-25 23:45:31 +0000

    task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strcmp -  concatenates two strings.
+ * @s1: parameter.
+ * @s2: parameter.
+ * Return: value of rest.
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+
+	if (*s1 == *s2)
+	{
+	return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+
+}
