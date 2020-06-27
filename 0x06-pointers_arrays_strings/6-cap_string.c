98bab1db270dd3793eb3a0944e13b57169fcfb77
Ktgtech <1903@holbertonschool.com>
2020-06-27 22:49:49 +0000

    task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,16 +26,15 @@ char *cap_string(char *s)
 		case ')':
 		case '{':
 		case '}':
-
-		if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 			s[i + 1] = s[i + 1] - 32;
 		}
 		s++;
 		i++;
 	}
-		if (s[0] >= 'a' && s[0] <= 'z')
-			s[0] = s[0] - 32;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
 
-		return (p);
+	return (p);
 
 }
