c23d291f684445e24487d9888f958fd029737713
Ktgtech <1903@holbertonschool.com>
2020-06-28 17:24:07 +0000

    task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _memset -  fills memory with a constant byte.
+ * @s: punter.
+ * @b: parameter.
+ * @n: parameter.
+ **/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif /* HOLBERTON_H */
