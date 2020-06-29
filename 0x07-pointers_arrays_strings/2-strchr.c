c4e7c5685ca31e35f89651911fb405b961a29fc6
Ktgtech <1903@holbertonschool.com>
2020-06-29 17:51:33 +0000

    task

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string..
+ * @s: punter parameter.
+ * @c: parameter.
+ * Return: s.
+ */
+char *_strchr(char *s, char c)
+{
+	char *aux;
+	unsigned int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			aux = &s[i];
+			return (aux);
+		}
+	}
+	return (s);
+}
