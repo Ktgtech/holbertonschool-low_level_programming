0fe5f8decdc3f9f1249cb331881d03691039bc04
Ktgtech <1903@holbertonschool.com>
2020-06-29 20:44:24 +0000

    task

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strpbrk - locates a character in a string..
+ * @s: punter parameter.
+ * @accept: parameter.
+ * Return: null or string.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, a;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (a = 0; *(s + a) != '\0'; a++)
+		{
+			if (*(s + i) == *(accept + a))
+				return (&s[i]);
+		}
+	}
+	return (0);
+}
