ce5da89b80c82a3603cda2ecea046fcdc581d75f
Ktgtech <1903@holbertonschool.com>
2020-06-29 19:22:12 +0000

    task2

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * strspn - gets the length of a prefix substring..
+ * @s: punter parameter.
+ * @accept: punter parameter.
+ * Return: su.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int su = 0;
+	int i, a;
+
+	for (i = 0; *(s + i) != 32; i++)
+	{
+		for (a = 0; *(accept + a)!= '\0'; a++)
+		{
+			if (s[i] == accept[a])
+			{
+				su = su +1;
+			}
+		}
+	}
+	return (su);
+}
