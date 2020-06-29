25cebd40e3dcd8ea69ff7ab47c38e2417d7a1861
Ktgtech <1903@holbertonschool.com>
2020-06-29 18:28:51 +0000

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,11 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
-		if (s[i] == c)
-		{
+		if (s[i] == c
 			break;
-		}
 
-		return (&s[i]);
+		if (s[i] == c)
+			return (&s[i]);
 
 	return (0);
 }
