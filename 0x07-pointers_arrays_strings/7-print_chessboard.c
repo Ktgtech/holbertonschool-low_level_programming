c975e92b13efab8e9d43cca94dde2e7b367cd4f5
Ktgtech <1903@holbertonschool.com>
2020-06-30 00:08:01 +0000

    task

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+B#include "holberton.h"
+/**
+ * print_chessboard - check the code for Holberton School students.
+ * @a: punter.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, d;
+
+	for (i = 0; i < 8 ; i++)
+	{
+		for (d = 0; d < 8; d++)
+		{
+			_putchar(a[i][d]);
+		}
+		_putchar('\n');
+	}
+}
