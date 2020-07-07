ea6d6eb25db0a0912629e23ceca0a4e79191b4a9
Ktgtech <1903@holbertonschool.com>
2020-07-07 22:06:22 +0000

    task 01

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars,initializes it with a specific char
+ * @size: parameter to the size of the array.
+ * @c: parameter character.
+ * Return: 0 for finish or pointer.
+ **/
+
+char *create_array(unsigned int size, char c)
+{
+
+	if (size == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		char *p = NULL;
+		unsigned int i = 0;
+
+		p = (char *)malloc(size * sizeof(char));
+
+		if (p)
+		{
+			for (i = 0; i < size; i++)
+				p[i] = c;
+			return (p);
+		}
+		else
+			return (0);
+	}
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#malloc, free
+This is a proyect in C, about malloc and free.
