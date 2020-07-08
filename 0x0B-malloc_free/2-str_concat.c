ee393423a798399f0366105c715d2140c6111a29
Ktgtech <1903@holbertonschool.com>
2020-07-08 03:03:53 +0000

    task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - copy of the string given as a parameter.
+ * @str: pointer parameter.
+ * Return: null or pointer.
+ **/
+char *str_concat(char *s1, char *s2)
+{
+	char *p = NULL;
+	int it , i1, i2, i , c;
+
+	for (i1 = 0; s1[i1] != '\0'; i1++)
+	{
+	}
+	for (i2 = 0 ;s2[i2] != '\0'; i2++)
+	{
+	}
+	it = i1 + i2;
+
+	p = (char *)malloc((it + 1)  * sizeof(char));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			p[i] = s1[i];
+		}
+		for (c = 0; s2[c]; c++)
+		{
+			p[i] = s2[c];
+			i++;
+		}
+		return (p);
+	}
+}
