3745fe175254cd6b2f0f943ba855a824856d3ebf
Ktgtech <1903@holbertonschool.com>
2020-07-08 03:08:13 +0000

    fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,34 +8,41 @@
  **/
 char *str_concat(char *s1, char *s2)
 {
-	char *p = NULL;
-	int it, i1, i2, i, c;
-
-	for (i1 = 0; s1[i1] != '\0'; i1++)
+	if (*s1 == NULL || *s2 == NULL)
 	{
-	}
-	for (i2 = 0 ; s2[i2] != '\0'; i2++)
-	{
-	}
-	it = i1 + i2;
-
-	p = (char *)malloc((it + 1)  * sizeof(char));
-
-	if (p == NULL)
-	{
-		return (NULL);
+		return (0);
 	}
 	else
 	{
-		for (i = 0; s1[i] != '\0'; i++)
+		char *p = NULL;
+		int it, i1, i2, i, c;
+
+		for (i1 = 0; s1[i1] != '\0'; i1++)
 		{
-			p[i] = s1[i];
 		}
-		for (c = 0; s2[c]; c++)
+		for (i2 = 0 ; s2[i2] != '\0'; i2++)
 		{
-			p[i] = s2[c];
-			i++;
 		}
-		return (p);
+		it = i1 + i2;
+
+		p = (char *)malloc((it + 1)  * sizeof(char));
+
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			for (i = 0; s1[i] != '\0'; i++)
+			{
+				p[i] = s1[i];
+			}
+			for (c = 0; s2[c]; c++)
+			{
+				p[i] = s2[c];
+				i++;
+			}
+			return (p);
+		}
 	}
 }
