6709371ceb7266959d0306af234b47884345ed32
Ktgtech <1903@holbertonschool.com>
2020-07-08 00:22:32 +0000

    task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strdup - copy of the string given as a parameter.
+ * @str: pointer parameter.
+ * Return: null or pointer.
+ **/
+
+
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		unsigned int i = 0;
+		unsigned int c = 0;
+		char *p = NULL;
+
+		for (i = 0; str[i] != '\0'; i++)
+		{
+		}
+		p = (char *)malloc(i * sizeof(char));
+
+		if (p)
+		{
+			for (c = 0; c < i; c++)
+			{
+				p[c] = str[c];
+			}
+			return (p);
+			free(str);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
