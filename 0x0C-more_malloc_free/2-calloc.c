46bccc2865ab3c4d5eab03728de157afac44df0a
Ktgtech <1903@holbertonschool.com>
2020-07-10 05:37:43 +0000

    holberton.h

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,5 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * _calloc - allocates memory
@@ -21,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!p)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < nmemb; i++)
 		p[i] = 0;
 
 	return (p);
