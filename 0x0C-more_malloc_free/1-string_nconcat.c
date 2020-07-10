805828367c8172b67282f726208e98ecb3048b2a
Ktgtech <1903@holbertonschool.com>
2020-07-10 03:02:58 +0000

    task

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,34 +16,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	i = i2 = leng = buf = 0;
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	for (i = 0; s1[i]; i++)
-
+	{
+	}
 	for (i2 = 0; s2[i2]; i2++)
-
+	{
+	}
 	if (n >= i2)
 		leng = i + i2;
 	else
 		leng = n + i;
-
 	p = (char *)malloc(leng * sizeof(char) + 2);
-
 	if (!p)
 		return (NULL);
 
 	for (i = 0; s1[i]; i++, buf++)
+	{
 		p[buf] = s1[i];
-
-	for (i2 = 0; i2 < n; i2++, buf++)
-		p[buf] = s2[i2];
-
+	}
+	if (n >= i2)
+	{
+		for (i2 = 0; s2[i2]; i2++, buf++)
+		{
+			p[buf] = s2[i2];
+		}
+	}
+	else
+		for (i2 = 0; i2 < n; i2++, buf++)
+		{
+			p[buf] = s2[i2];
+		}
 	p[buf] = '\0';
 	return (p);
 }
