eefbcc5a0189e744349a261de14747495c58bc8a
Ktgtech <1903@holbertonschool.com>
2020-07-10 06:20:49 +0000

    task

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * array_range - allocates memory
+ * @min: array parameter.
+ * @max: array parameter.
+ * Return: Null or p.
+ **/
+int *array_range(int min, int max)
+{
+	int *p = NULL;
+	int i, res;
+
+	if (min > max)
+		return (NULL);
+
+	res = max - min;
+	p = (int *)malloc(res * (int) + 1);
+
+	if (!p)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+		p[i] = min;
+
+	return (p);
+}
