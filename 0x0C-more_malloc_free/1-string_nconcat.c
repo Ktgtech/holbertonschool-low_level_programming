0d4e616c3ae840c4064333efa4f2329081dc09f1
Ktgtech <1903@holbertonschool.com>
2020-07-10 01:55:22 +0000

    task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - allocates memory
+ * @s1: array parameter.
+ * @s2: array parameter.
+ * @n: size of s2.
+ * Return: Null or p.
+ **/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i, i2, leng, buf;
+
+	i = i2 = leng = buf = 0;
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+
+	for (i = 0; s1[i]; i++)
+
+	for (i2 = 0; s2[i2]; i2++)
+
+	if (n >= i2)
+		leng = i + i2;
+	else
+		leng = n + i;
+
+	p = (char *)malloc(leng * sizeof(char) + 1);
+
+	if (!p)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++, buf++)
+		p[buf] = s1[i];
+
+	for (i2 = 0; i2 <= n; i2++, buf++)
+		p[buf] = s2[i2];
+
+return (p);
+}
