7f51e32ae926f06f39fb03729014fc84371981b1
Ktgtech <1903@holbertonschool.com>
2020-07-14 15:57:59 +0000

    task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,65 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * dog_t - function that initialize a variable of type struct dog.
+ * @new_dog: pointer.
+ * @name: Dog's name.
+ * @age: Dog's age.
+ * @owner: Dog's owner.
+ **/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+	int i, i2, i3, i4;
+
+	my_dog = malloc(sizeof(dog_t));
+
+	if (!my_dog)
+		return (NULL);
+
+	for (i4 = 0; name[i4]; i4++)
+		;
+	for (i3 = 0; owner[i3]; i3++)
+		;
+
+	my_dog->age = age;
+
+	my_dog->name = malloc((i4 + 1) * sizeof(char));
+	if (my_dog->name)
+	{
+		for (i = 0; i < i4; i++)
+		{
+			my_dog->name[i] = name[i];
+		}
+
+		my_dog->name[i] = '\0';
+	}
+	else
+	{
+		free(my_dog);
+		return (NULL);
+	}
+
+	my_dog->owner = malloc((i3 + 1) * sizeof(char));
+	if (my_dog->owner)
+	{
+		for (i2 = 0; i2 < i3; i2++)
+		{
+			my_dog->owner[i2] = owner[i2];
+		}
+
+		my_dog->owner[i2] = '\0';
+	}
+
+	else
+	{
+		free(my_dog->name);
+		free(my_dog->owner);
+		free(my_dog);
+		return (NULL);
+	}
+
+	return (my_dog);
+}
