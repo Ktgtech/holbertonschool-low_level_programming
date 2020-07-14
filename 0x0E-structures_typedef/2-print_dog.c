06f933ba61f212b03dd8d8fed4068b386a77efd5
Ktgtech <1903@holbertonschool.com>
2020-07-14 03:27:06 +0000

    task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,19 +9,19 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if ((d->name == NULL))
-			printf("Name: nil\n");
-		else
+		if ((d->name))
 			printf("my_dog.name = %s\n", d->name);
-
-		if ((d->age == 0))
-			printf("nil\n");
 		else
+			printf("Name: nil\n");
+
+		if ((d->age))
 			printf("my_dog.age = %f\n", d->age);
-
-		if ((d->owner == NULL))
-			printf("nil\n");
 		else
+			printf("nil\n");
+
+		if ((d->owner))
 			printf("my_dog.owner = %s\n", d->owner);
+		else
+			printf("nil\n");
 	}
 }
